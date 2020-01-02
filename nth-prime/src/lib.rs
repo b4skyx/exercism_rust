4a8a07785c0e2b724ac0081520dc1044661e036b
b4skyx@gmail.com <b4skyx@gmail.com>
2020-01-02 22:59:39 +0530

    nth_prime

---
+++ nth-prime/src/lib.rs
@@ -0,0 +1,26 @@
+pub fn nth(n: u32) -> u32 {
+    if n == 0 {
+        return 2;
+    }
+    if n == 1 {
+        return 3;
+    }
+    let mut count: u32 = 1;
+    let mut current: u32 = 3;
+    loop {
+        current = current + 1;
+        let mut prime: bool = true;
+        for r in 2..(current / 2) {
+            if current % r == 0 {
+                prime = false;
+                break;
+            }
+        }
+        if prime == true {
+            if count == n {
+                return current;
+            }
+            count = count + 1;
+        }
+    }
+}
