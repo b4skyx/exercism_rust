ab80b1e13329f9ddd07d6ca0351d75ba7f97d2f1
b4skyx@gmail.com <b4skyx@gmail.com>
2020-01-02 21:22:23 +0530

    gigasecond_fix

--- gigasecond/src/lib.rs
+++ gigasecond/src/lib.rs
@@ -2,7 +2,7 @@ use chrono::Duration;
 use chrono::{DateTime, Utc};
 // Returns a Utc DateTime one billion seconds after start.
 pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
-    let gigasecond = Duration::seconds(1000000000);
-    let end: DateTime<Utc> = start.checked_add_signed(gigasecond).unwrap();
+    let gigasecond = Duration::seconds(1_000_000_000);
+    let end: DateTime<Utc> = start+gigasecond;
     return end;
 }
