b2918a5a5e50aa1cad2df22fb172901eda676e62
b4skyx@gmail.com <b4skyx@gmail.com>
2020-01-02 11:22:57 +0530

    modified:   gigasecond/src/lib.rs 	modified:   leap/src/lib.rs

--- gigasecond/src/lib.rs
+++ gigasecond/src/lib.rs
@@ -1,9 +1,8 @@
-use chrono::{DateTime, Utc};
 use chrono::Duration;
+use chrono::{DateTime, Utc};
 // Returns a Utc DateTime one billion seconds after start.
 pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
     let gigasecond = Duration::seconds(1000000000);
     let end: DateTime<Utc> = start.checked_add_signed(gigasecond).unwrap();
     return end;
 }
-
