2bf8c3d1bdedc1a76bcd693865d81b58e7985ff4
b4skyx@gmail.com <b4skyx@gmail.com>
2020-01-06 11:26:50 +0530

    modified:   beer-song/src/lib.rs

--- beer-song/src/lib.rs
+++ beer-song/src/lib.rs
@@ -1,7 +1,23 @@
 pub fn verse(n: u32) -> String {
-    unimplemented!("emit verse {}", n)
+    let ret : String;
+    if n == 2{
+    ret = format!("{} bottles of beer on the wall, {} bottles of beer.\nTake one down and pass it around, {} bottle of beer on the wall.\n", n, n, n-1);
+    }
+    else if n == 1{
+    ret = format!("{} bottle of beer on the wall, {} bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n", n, n);
+    }
+    else if n == 0{
+    ret = format!("No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n");
+    }
+    else{ret=format!("{} bottles of beer on the wall, {} bottles of beer.\nTake one down and pass it around, {} bottles of beer on the wall.\n", n, n, n-1);}
+    return ret;
 }
-
 pub fn sing(start: u32, end: u32) -> String {
-    unimplemented!("sing verses {} to {}, inclusive", start, end)
+    let ret:String=verse(start);
+    if start > end{
+        return format!("{}\n{}",ret,sing(start-1,end));
+    }
+    else{
+    return ret;
+    }
 }
