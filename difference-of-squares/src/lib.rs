100f22324ed492f8119ee5bddadc96692e866c0a
b4skyx@gmail.com <b4skyx@gmail.com>
2020-01-10 20:11:10 +0530

    modified:   difference-of-squares/src/lib.rs

--- difference-of-squares/src/lib.rs
+++ difference-of-squares/src/lib.rs
@@ -1,19 +1,18 @@
 pub fn square_of_sum(n: u32) -> u32 {
-    let mut res: u32 = 0;
-    for i in 1..(n + 1) {
-        res = res + i;
-    }
-    return res * res;
+    let mut res:u32 = 0;
+    (1..(n+1)).for_each(|x| res=res+x);
+    res*res
 }
 
 pub fn sum_of_squares(n: u32) -> u32 {
-    if n == 1 {
+    if n>1{
+        return n*n+sum_of_squares(n-1);
+    }
+    else{
         return 1;
-    } else {
-        return (n * n) + sum_of_squares(n - 1);
     }
 }
 
 pub fn difference(n: u32) -> u32 {
-    return square_of_sum(n) - sum_of_squares(n);
+    square_of_sum(n)-sum_of_squares(n)
 }
