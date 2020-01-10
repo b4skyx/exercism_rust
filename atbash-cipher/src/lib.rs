492dd8ae160c609158303dcaa48f48b3e263e316
b4skyx@gmail.com <b4skyx@gmail.com>
2020-01-10 19:40:31 +0530

    modified:   src/lib.rs 	../sum-of-multiples/

--- atbash-cipher/src/lib.rs
+++ atbash-cipher/src/lib.rs
@@ -1,40 +1,24 @@
 /// "Encipher" with the Atbash cipher.
-pub fn encode(plain: &str) -> String {
-    let mut cipher:String = String::new();
-    let mut count:u16 = 0;
-    for ch in plain.to_lowercase().chars(){
-        if ch >= 'a' && ch <= 'z'
-        {
-            if count == 5{
-                cipher.push(' ');
-                count=0;
-            }
-            cipher.push((('z' as u8)- (ch as u8 - 'a' as u8)) as char);
-            count = count + 1;
-        }
-        else if ch >= '0' && ch <= '9'{
-            cipher.push(ch);
-            count = count+1;
-            if count == 5{
-                cipher.push(' ');
-                count=0;
-            }
-        }
+pub fn convert(text: &str, gap:usize) -> String{
+    let mut cnvrt:String = String::new();
+    text.to_lowercase().chars()
+        .filter(|x| x.is_alphanumeric())
+        .for_each(|ch| if ch.is_alphabetic(){ cnvrt.push((('z' as u8)- (ch as u8 - 'a' as u8)) as char)} else { cnvrt.push(ch)});
+    if gap > 0{
+        let length = cnvrt.len();
+    for i in (gap..(length -1 + (length/gap) )).step_by(gap+1){
+        cnvrt.insert(i, ' ');
     }
-    return cipher;
+    }
+    return cnvrt;
+}
+
+
+pub fn encode(plain: &str) -> String {
+    convert(plain,5)
 }
 
 /// "Decipher" with the Atbash cipher.
 pub fn decode(cipher: &str) -> String {
-    let mut plain:String = String::new();
-    for ch in cipher.chars(){
-        if ch >= 'a' && ch <= 'z'
-        {
-            plain.push((('z' as u8)- (ch as u8 - 'a' as u8)) as char);
-        }
-        else if ch >= '0' && ch <= '9'{
-            plain.push(ch);
-        }
-    }
-    return plain;
+    convert(cipher,0)
 }
