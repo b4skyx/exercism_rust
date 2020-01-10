bf36eca9f6d97c2eb2fe2d9aa0fdb4a1352d0b8d
b4skyx@gmail.com <b4skyx@gmail.com>
2020-01-10 19:41:01 +0530

    new file:   sum-of-multiples/.gitignore 	new file:   sum-of-multiples/Cargo.toml 	new file:   sum-of-multiples/README.md 	new file:   sum-of-multiples/src/lib.rs 	new file:   sum-of-multiples/tests/sum-of-multiples.rs

---
+++ sum-of-multiples/src/lib.rs
@@ -0,0 +1,13 @@
+pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
+    let mut result:u32 = 0;
+    let mut fct:bool=true;
+    for i in 1..limit{
+        for r in factors.iter().skip_while(|x| (x as u32) ==0u32){
+            if i%r == 0 {
+                result=result+i;
+                break;
+            }
+        }
+    }
+    return result;
+}
