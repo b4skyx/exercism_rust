2fe8add63dc68ac36532565e37d371e28bb8286a
Abhishek Yadav <b4skyx@gmail.com>
2020-01-27 18:24:59 +0530

    modified:   src/lib.rs

--- prime-factors/src/lib.rs
+++ prime-factors/src/lib.rs
@@ -1,3 +1,22 @@
 pub fn factors(n: u64) -> Vec<u64> {
-    unimplemented!("This should calculate the prime factors of {}", n)
+    let mut factors : Vec<u64> = Vec::new();
+    let mut res: u64 = n;
+    if n < 2{
+        return factors;}
+    if n == 2{
+        factors.push(2);
+        return factors;
+    }
+    loop{
+        for i in 2..n{
+            if res%i == 0{
+                factors.push(i);
+                res = res / i;
+                break;
+            }}
+        if res == 1{
+            break;
+        }
+    }
+    factors
 }
