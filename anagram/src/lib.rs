642c04da7e2ffa03d800de671751d04d208e9947
Abhishek Yadav <b4skyx@gmail.com>
2020-01-31 22:38:12 +0530

    modified:   anagram/src/lib.rs 	new file:   armstrong-numbers/.gitignore 	new file:   armstrong-numbers/Cargo.toml 	new file:   armstrong-numbers/README.md 	new file:   armstrong-numbers/src/lib.rs 	new file:   armstrong-numbers/tests/armstrong-numbers.rs

--- anagram/src/lib.rs
+++ anagram/src/lib.rs
@@ -1,5 +1,27 @@
 use std::collections::HashSet;
+use std::collections::HashMap;
 
-pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&str]) -> HashSet<&'a str> {
+fn frequency_hash( word: &str ) -> HashMap<char, u32>{
+    let mut freq : HashMap<char, u32> = HashMap::new();
+    for character in word.chars(){
+        let counter = freq.entry(character).or_insert(0);
+        *counter += 1;
+    }
+    freq
+}
 
+pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
+    let mut result = HashSet::new();
+    let lowercased_word = word.to_lowercase();
+    let word_hash = frequency_hash(&lowercased_word);
+    for &possible in possible_anagrams.iter(){
+        let lowercased_possible = possible.to_lowercase();
+        if lowercased_possible == lowercased_word{
+            continue;
+        }
+        else if word_hash == frequency_hash(&lowercased_possible){
+                result.insert(possible);
+        }
+    }
+    result
 }
