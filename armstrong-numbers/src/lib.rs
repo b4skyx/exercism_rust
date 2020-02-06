42badd71c252b1fcc0275ccc6ea4fd0e144cf0d7
Abhishek Yadav <b4skyx@gmail.com>
2020-02-06 19:26:24 +0530

    modified:   armstrong-numbers/src/lib.rs

--- armstrong-numbers/src/lib.rs
+++ armstrong-numbers/src/lib.rs
@@ -1,3 +1,14 @@
 pub fn is_armstrong_number(num: u32) -> bool {
-    unimplemented!("true if {} is an armstrong number", num)
+    let mut sum = 0;
+    let temp = num.to_string();
+
+    for ch in temp.chars(){
+        sum = sum + ((ch.to_digit(10)).unwrap()).pow(temp.chars().count() as u32);
+    }
+    if sum == num{
+        true
+    }
+    else{
+        false
+    }
 }
