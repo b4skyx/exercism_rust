040c8017b20dcee8f2fe43fbbcd39c2f4d8b0827
Abhishek Yadav <b4skyx@gmail.com>
2020-03-21 18:05:30 +0530

    modified:   src/lib.rs

--- bob/src/lib.rs
+++ bob/src/lib.rs
@@ -1,3 +1,31 @@
 pub fn reply(message: &str) -> &str {
-    unimplemented!("have Bob reply to the incoming message: {}", message)
+    let mut msg = message.to_string().trim().to_string();
+    let mut alphabetic = false;
+    let mut yell = false;
+    let mut question = false;
+    for alph in msg.chars() {
+        if alph.is_alphabetic() {
+            alphabetic = true;
+            break;
+        }
+    }
+    if msg.to_uppercase() == msg && alphabetic == true {
+        yell = true;
+    }
+    if msg.ends_with("?") {
+        question = true;
+    }
+    let mut result: &str = "";
+    if msg.len() == 0 {
+        result = "Fine. Be that way!";
+    } else if yell == true && question == true {
+        result = "Calm down, I know what I'm doing!";
+    } else if yell == true && question == false {
+        result = "Whoa, chill out!";
+    } else if yell == false && question == true {
+        result = "Sure.";
+    } else {
+        result = "Whatever.";
+    }
+    result
 }
